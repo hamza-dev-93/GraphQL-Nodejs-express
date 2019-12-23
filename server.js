1b2fb992c45c150530ee22579bfa7b8097a9fade
hamza77500 <bedwihamza@gmail.com>
2019-12-23 11:17:03 +0100

    init example of node js express and graphQL

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+// Graph Schema
+
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Root resolver
+var root = {
+    message: () => 'hello grapheQL!!'
+};
+
+// Create an express server and GraphQL endpoint
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(4000, () => console.log('Express GraphQL Server Now running On localhos:4000/graphql'));
+
